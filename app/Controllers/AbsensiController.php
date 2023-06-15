c485e10b16c93cc590d5c143f3300f27d52f1c3a
ruligandari <ruligandari@gmail.com>
2023-06-16 00:55:02 +0700

    fix presensi belum ada

--- app/Controllers/AbsensiController.php
+++ app/Controllers/AbsensiController.php
@@ -17,6 +17,8 @@ class AbsensiController extends BaseController
     {
         $status = '';
         $id_waktu_presensi = '';
+        $namaMk = '';
+
         date_default_timezone_set('Asia/Jakarta');
         $namaMhs = $id;
         $parts = explode(" ", $namaMhs);
@@ -30,21 +32,29 @@ class AbsensiController extends BaseController
         foreach ($dataMahasiswa as $mahasiswa) {
             $jam_masuk = strtotime($mahasiswa['jam_masuk']);
             $jam_keluar = strtotime($mahasiswa['jam_keluar']);
-            if ($time >= $jam_masuk && $time <= $jam_keluar && $mahasiswa['status'] == 'tidak hadir') {
+            if ($date == $mahasiswa['tanggal'] && $time >= $jam_masuk && $time <= $jam_keluar && $mahasiswa['status'] == 'tidak hadir') {
             $status = 'hadir';
             $namaMk = $mahasiswa['nama_mk'];
             $id_waktu_presensi = $mahasiswa['id'];
+
             } else if ($time >= $jam_keluar) {
             $status = 'tidak hadir';
             $namaMk = $mahasiswa['nama_mk'];
             }
         }
 
+        $data = [
+            'status' => 'error',
+            'msg' => 'Belum Ada Presensi ',
+        ];
+
+        
         if ($status == 'hadir'){
+            return json_encode(array('status' => 'success', 'msg' => 'Presensi  Berhasil'));
             $presensiData->updateStatusByIdWaktuPresensi($id_waktu_presensi, $nim);
-            return json_encode(array('status' => 'success', 'msg' => 'Presensi '.$namaMk. ' Berhasil'));
         } else if ($status == 'tidak hadir'){
             return json_encode(array('status' => 'error', 'msg' => 'Anda Sudah Melakukan Presensi '.$namaMk. ' Pada Tanggal '. $date. ' Jam '. date('H:i:s')));
         }
+        return json_encode($data);
     }
 }
