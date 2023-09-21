a3e9b532fc84705a4c3cef2cb6b29e458d39fdf5
ruligandari <ruligandari@gmail.com>
2023-09-21 17:29:14 +0700

    fix proses absensi dan hapus mahasiswa

--- app/Views/Admin/mahasiswa/index.php
+++ app/Views/Admin/mahasiswa/index.php
@@ -1,6 +1,6 @@
-<?= $this->extend('layout/dashboard-admin')?>
+<?= $this->extend('layout/dashboard-admin') ?>
 
-<?= $this->section('content')?>
+<?= $this->section('content') ?>
 <div class="content-wrapper">
   <div class="row">
     <div class="col-sm-12">
@@ -13,24 +13,23 @@
                   <div class="col-12 grid-margin stretch-card">
                     <div class="card card-rounded">
                       <div class="card-body">
-                          <?php 
-                                if (session()->getFlashdata('success')) {
-                                    echo '<div class="alert alert-success" role="alert">'.session()->getFlashdata('success').'</div>';
-                                } else if (session()->getFlashdata('error')){
-                                    echo '<div class="alert alert-danger" role="alert">'.session()->getFlashdata('error').'</div>';
-                                }
-                                ?>
+                        <?php
+                        if (session()->getFlashdata('success')) {
+                          echo '<div class="alert alert-success" role="alert">' . session()->getFlashdata('success') . '</div>';
+                        } else if (session()->getFlashdata('error')) {
+                          echo '<div class="alert alert-danger" role="alert">' . session()->getFlashdata('error') . '</div>';
+                        }
+                        ?>
                         <div class="d-sm-flex justify-content-between align-items-start">
                           <div>
                             <h4 class="card-title card-title-dash">Kelola Data Mahasiswa</h4>
                           </div>
                           <div>
-                            <a href="<?=base_url('admin/mahasiswa/create')?>" type="button"
-                              class="btn btn-success text-white">Tambah Data</a>
+                            <a href="<?= base_url('admin/mahasiswa/create') ?>" type="button" class="btn btn-success text-white">Tambah Data</a>
                           </div>
                         </div>
                         <div class="table-responsive">
-                          <table  id="mhs" class="table table-hover" width="100%">
+                          <table id="mhs" class="table table-hover" width="100%">
                             <thead>
                               <tr>
                                 <th>No</th>
@@ -44,31 +43,28 @@
                               </tr>
                             </thead>
                             <tbody>
-                              <?php $no = 1?>
-                              <?php foreach ($mahasiswa as $row): ?>
+                              <?php $no = 1 ?>
+                              <?php foreach ($mahasiswa as $row) : ?>
                                 <tr>
-                                  <td><?=$no++?></td>
-                                  <td><?=$row['nim']?></td>
-                                  <td><?=$row['nama']?></td>
-                                  <td><?=$row['kelas']?></td>
-                                  <td><?=$row['jurusan']?></td>
-                                  <td><?=$row['jenis_kelamin']?></td>
-                                  <td> 
-                                    <?php if ($row['foto']): ?>
-                                    <img src="<?= base_url('uploads/' . $row['foto']) ?> ">
-                                    <?php else: ?>
-                                        Belum Ada Foto
+                                  <td><?= $no++ ?></td>
+                                  <td><?= $row['nim'] ?></td>
+                                  <td><?= $row['nama'] ?></td>
+                                  <td><?= $row['kelas'] ?></td>
+                                  <td><?= $row['jurusan'] ?></td>
+                                  <td><?= $row['jenis_kelamin'] ?></td>
+                                  <td>
+                                    <?php if ($row['foto']) : ?>
+                                      <img src="<?= base_url('uploads/' . $row['foto']) ?> ">
+                                    <?php else : ?>
+                                      Belum Ada Foto
                                     <?php endif; ?>
                                   <td>
-                                    <a href="<?= base_url('admin/mahasiswa/form_edit/'). $row['nim']?>"
-                                      class="btn-sm btn-primary text-white"> <i class="fas fa-edit"></i></a>
-                                      <button data-bs-target="#hapusModal<?=$row['nim']?>" data-bs-toggle="modal"
-                                      class="btn-sm btn-danger text-white"><i class="fas fa-trash"></i></button>
-                                      <a href="<?= base_url('admin/mahasiswa/pas-foto/'). $row['nim']?>"
-                                        class="btn-sm btn-success"><i class="fas fa-camera"></i></a>
+                                    <a href="<?= base_url('admin/mahasiswa/form_edit/') . $row['nim'] ?>" class="btn-sm btn-primary text-white"> <i class="fas fa-edit"></i></a>
+                                    <button data-bs-target="#hapusModal<?= $row['nim'] ?>" data-bs-toggle="modal" class="btn-sm btn-danger text-white"><i class="fas fa-trash"></i></button>
+                                    <a href="<?= base_url('admin/mahasiswa/pas-foto/') . $row['nim'] ?>" class="btn-sm btn-success"><i class="fas fa-camera"></i></a>
                                   </td>
                                 </tr>
-                                <?php endforeach?>
+                              <?php endforeach ?>
                             </tbody>
                           </table>
                         </div>
@@ -84,40 +80,40 @@
     </div>
   </div>
 </div>
-<?php foreach ($mahasiswa as $mk) :?>
-        <div class="modal fade" id="hapusModal<?=$mk['nim']?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Hapus?</h5>
-                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <form action="<?= base_url('admin/mahasiswa/delete/'). $row['nim']?>" method="POST">
-              <?= csrf_field()?>
-              <div class="modal-body">
-                Apakah anda yakin ingin menghapus Mahasiswa <?= $row['nama']?> ?
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-danger" id="hapusDataBtn">Delete</button>
-              </div>
-              </form>
-            </div>
-          </div>
+<?php foreach ($mahasiswa as $mk) : ?>
+  <div class="modal fade" id="hapusModal<?= $mk['nim'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Hapus?</h5>
+          <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
         </div>
-<?php endforeach?>
-<?= $this->endSection()?>
+        <form action="<?= base_url('admin/mahasiswa/delete/') . $mk['nim'] ?>" method="POST">
+          <?= csrf_field() ?>
+          <div class="modal-body">
+            Apakah anda yakin ingin menghapus Mahasiswa <?= $mk['nama'] ?> ?
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-danger" id="hapusDataBtn">Delete</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+<?php endforeach ?>
+<?= $this->endSection() ?>
 
-<?= $this->section('script')?>
-  <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-  <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<?= $this->section('script') ?>
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 
 <script>
-  $(document).ready(function () {
+  $(document).ready(function() {
     $('#mhs').DataTable();
-});
+  });
 </script>
 <script>
   // Menangani klik tombol hapus pada modal
@@ -130,4 +126,4 @@
     $('#hapusModal').modal('hide');
   });
 </script>
-<?= $this->endSection()?>
+<?= $this->endSection() ?>
--- app/Views/presensi.php
+++ app/Views/presensi.php
@@ -1,45 +1,45 @@
-<?= $this->extend('layout/auth-login-admin')?>
+<?= $this->extend('layout/auth-login-admin') ?>
 
-<?= $this->section('header');?>
+<?= $this->section('header'); ?>
 <style>
-    video{
-        position: absolute;
-    }
+  video {
+    position: absolute;
+  }
 
-    canvas {
-        position: relative;
-    }
+  canvas {
+    position: relative;
+  }
 </style>
-<?= $this->endSection();?>
+<?= $this->endSection(); ?>
 
-<?= $this->section('content')?>
+<?= $this->section('content') ?>
 
 <div class="content-wrapper d-flex align-items-center auth px-0">
-    <div class="row w-100 mx-0">
-        <div class="col-lg-5 mx-auto">
-            <h2 class="text-center mb-3">Sistem Presensi Face Recognition</h2>
-            <!-- <video autoplay="true" id="webcam-video"></video> -->
-            
-                <div id="video" class="video"></div>
-            
-            <div class="label text-center"><span>Tahan Posisi Dalam Beberapa Detik</span></div>
-            <div class="label text-center" id="label"></div>
-        </div>
+  <div class="row w-100 mx-0">
+    <div class="col-lg-5 mx-auto">
+      <h2 class="text-center mb-3">Sistem Presensi Face Recognition</h2>
+      <!-- <video autoplay="true" id="webcam-video"></video> -->
+
+      <div id="video" class="video"></div>
+
+      <div class="label text-center"><span>Tahan Posisi Dalam Beberapa Detik</span></div>
+      <div class="label text-center" id="label"></div>
     </div>
+  </div>
 </div>
 
-<?= $this->endSection()?>
+<?= $this->endSection() ?>
 
-<?= $this->section('script')?>
-<script src="<?= base_url('js/face-api.min.js')?>"></script>
+<?= $this->section('script') ?>
+<script src="<?= base_url('js/face-api.min.js') ?>"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
   // Fetch model from server
-  fetch('<?=base_url('model')?>')
+  fetch('<?= base_url('model') ?>')
     .then(response => response.json())
     .then(data => {
       // Load the face recognition model
-      var models = '<?=base_url('models')?>'
+      var models = '<?= base_url('models') ?>'
       Promise.all([
         faceapi.nets.tinyFaceDetector.loadFromUri(models),
         faceapi.nets.faceLandmark68Net.loadFromUri(models),
@@ -48,7 +48,9 @@
 
       function startFaceRecognition() {
         // Start webcam stream
-        navigator.mediaDevices.getUserMedia({ video: true })
+        navigator.mediaDevices.getUserMedia({
+            video: true
+          })
           .then(stream => {
             const video = document.createElement('video');
             video.id = 'video';
@@ -66,46 +68,61 @@
               canvas.width = 640;
               canvas.height = 480;
               document.getElementById('video').appendChild(canvas);
-              const displaySize = { width: video.width, height: video.height };
+              const displaySize = {
+                width: video.width,
+                height: video.height
+              };
               faceapi.matchDimensions(canvas, displaySize);
 
               // Perform face recognition on each video frame
+              let isDetectionPaused = false;
+
               setInterval(async () => {
-                const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-                  .withFaceLandmarks()
-                  .withFaceDescriptors();
-                 const labeledDescriptors = data.labels.map((label, index) => {
+                if (!isDetectionPaused) {
+                  const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+                    .withFaceLandmarks()
+                    .withFaceDescriptors();
+                  const labeledDescriptors = data.labels.map((label, index) => {
                     const descriptors = Float32Array.from(data.descriptors[index]);
                     return new faceapi.LabeledFaceDescriptors(label, [descriptors]);
-                    });
-                
-                const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors);
-                const results = detections.map(detection => faceMatcher.findBestMatch(detection.descriptor));
-                // Draw bounding boxes and labels on canvas
-                canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-                results.forEach((result, i) => {
-                  const box = detections[i].detection.box;
-                  const label = result.toString();
-                  const drawBox = new faceapi.draw.DrawBox(box, { label });
-                  drawBox.draw(canvas);
+                  });
 
-                  // Update label text
-                  const labelElement = document.getElementById('label');
-                  labelElement.innerHTML = label;
-                  labeled = label.substring(0, label.indexOf(' '));
-                  if (labeled != 'unknown') {
-                      presensi(label)
-                  }else {
-                    Swal.fire({
-                      text: 'Wajah Tidak Terdaftar!',
-                      icon: 'error',
-                      showConfirmButton: false,
-                      timer: 3000
-                    })
-                  }
-                });
+                  const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors);
+                  const results = detections.map(detection => faceMatcher.findBestMatch(detection.descriptor));
 
-              }, 5000);
+                  // Draw bounding boxes and labels on canvas
+                  canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+                  results.forEach(async (result, i) => {
+                    if (!isDetectionPaused) {
+                      const box = detections[i].detection.box;
+                      const label = result.toString();
+                      const drawBox = new faceapi.draw.DrawBox(box, {
+                        label
+                      });
+                      drawBox.draw(canvas);
+
+                      // Update label text
+                      const labelElement = document.getElementById('label');
+                      labelElement.innerHTML = label;
+                      labeled = label.substring(0, label.indexOf(' '));
+                      if (labeled != 'unknown') {
+                        isDetectionPaused = true; // Menandakan bahwa deteksi dihentikan
+                        presensi(label);
+                        setTimeout(() => {
+                          isDetectionPaused = false; // Setelah 5 detik, deteksi dilanjutkan
+                        }, 5000);
+                      } else {
+                        Swal.fire({
+                          text: 'Wajah Tidak Terdaftar!',
+                          icon: 'error',
+                          showConfirmButton: false,
+                          timer: 3000
+                        })
+                      }
+                    }
+                  });
+                }
+              }, 1000);
             };
           })
           .catch(error => {
@@ -113,9 +130,9 @@
           });
       }
 
-      function presensi(id){
+      function presensi(id) {
         $.ajax({
-          url: "<?=base_url('absen/')?>"+id,
+          url: "<?= base_url('absen/') ?>" + id,
           type: "GET",
           success: function(response) {
             console.log(response);
@@ -135,4 +152,4 @@
       console.log('Error fetching model:', error);
     });
 </script>
-<?= $this->endSection()?>
+<?= $this->endSection() ?>
